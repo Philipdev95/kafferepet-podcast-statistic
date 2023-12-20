198e1943cf560d44164283fd079141ca9a81b2e0
Philipdev95 <philipdev95@gmail.com>
2023-12-20 11:44:28 +0100

    ta bort ngZone som inte används

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'zone.js';
 
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
   public secondArr: any
   public thirdArr: any;
   public episodeCount = 0
-  constructor(private httpClient: HttpClient, private ngZone: NgZone) {}
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
     let arr: any = []
